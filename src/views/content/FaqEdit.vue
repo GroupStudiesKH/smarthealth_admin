7e5108f14b8ca3169e9e33656707eaadf15ddcce
mafiapatch <g8071107@gmail.com>
2025-03-25 20:36:59 +0800

    refactor(FaqEdit.vue): 重构编辑器初始化逻辑并添加图片上传功能
    重构了编辑器的初始化逻辑，将编辑器创建移至 `onMounted` 钩子中，并添加了自定义图片上传适配器。移除了不必要的代码，提升了代码的可维护性和功能完整性。

--- src/views/content/FaqEdit.vue
+++ src/views/content/FaqEdit.vue
@@ -6,6 +6,7 @@ import Navbar from "@/components/Navbar.vue";
 import Sidebar from "@/components/Sidebar.vue";
 import apiService from "@/service/api-service.js";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import UploadAdapter from "@/utils/UploadAdapter";
 
 export default {
   components: {
@@ -35,18 +36,6 @@ export default {
       try {
         const response = await apiService.getPost({ id: route.params.id });
         currentFaq.value = { ...response };
-
-        editor.value = await ClassicEditor.create(
-          document.querySelector("#editor")
-        ).then((editor) => {
-          editor.setData(response.content);
-          content.value = editor.getData();
-          editor.model.document.on("change:data", () => {
-            content.value = editor.getData();
-          });
-          return editor;
-        });
-
       } catch (error) {
         console.error("Error fetching FAQ:", error);
         alert("獲取FAQ資料失敗，請稍後再試");
@@ -54,9 +43,6 @@ export default {
     };
 
     const saveFaq = async () => {
-      if (editor.value) {
-        currentFaq.value.content = content.value;
-      }
       try {
         if (!route.params.id) {
           const response = await apiService.createPost(currentFaq.value);
@@ -79,21 +65,25 @@ export default {
         };
       } catch (error) {
         console.error("Failed to save member:", error);
-        
+
         // 處理後端回傳的驗證錯誤
-        if (error.response?.data?.status === 'error') {
+        if (error.response?.data?.status === "error") {
           // 處理後端回傳的驗證錯誤
           if (error.response.data.content) {
             // 移除錯誤訊息中的括號和引號
             const cleanedErrors = {};
-            for (const [key, value] of Object.entries(error.response.data.content)) {
-              cleanedErrors[key] = Array.isArray(value) ? value[0].replace(/[\[\]"]/g, '') : value;
+            for (const [key, value] of Object.entries(
+              error.response.data.content
+            )) {
+              cleanedErrors[key] = Array.isArray(value)
+                ? value[0].replace(/[\[\]"]/g, "")
+                : value;
             }
             errors.value = cleanedErrors;
-            
+
             // 組合所有錯誤訊息
-            const errorMessages = Object.values(cleanedErrors).join('<br/>');
-            
+            const errorMessages = Object.values(cleanedErrors).join("<br/>");
+
             modalMessage.value = errorMessages;
             showModal.value = true;
           } else {
@@ -124,10 +114,65 @@ export default {
       }
     };
 
-
-    onMounted(() => {
-      editFaq();
+    onMounted(async () => {
+      await editFaq();
+      editor.value = await ClassicEditor.create(
+        document.querySelector("#editor"),
+        {
+          removePlugins: ["Markdown"],
+          extraPlugins: [MyCustomUploadAdapterPlugin],
+          toolbar: [
+            "heading",
+            "|",
+            "bold",
+            "italic",
+            "link",
+            "bulletedList",
+            "numberedList",
+            "blockQuote",
+            "imageUpload",
+          ],
+          heading: {
+            options: [
+              {
+                model: "paragraph",
+                title: "Paragraph",
+                class: "ck-heading_paragraph",
+              },
+              {
+                model: "heading1",
+                view: "h2",
+                title: "Heading 1",
+                class: "ck-heading_heading1",
+              },
+              {
+                model: "heading2",
+                view: "h3",
+                title: "Heading 2",
+                class: "ck-heading_heading2",
+              },
+            ],
+          },
+        }
+      )
+        .then((editorInstance) => {
+          editorInstance.model.document.on("change:data", () => {
+            currentFaq.value.content = editorInstance.getData();
+          });
+          return editorInstance;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     });
+
+    function MyCustomUploadAdapterPlugin(editor) {
+      editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+        return new UploadAdapter(loader);
+      };
+    }
+
+
 
     return {
       faqList,
@@ -138,7 +183,7 @@ export default {
       deleteFaq,
       showModal,
       modalMessage,
-      errors
+      errors,
     };
   },
 };
@@ -166,8 +211,7 @@ export default {
                   </div>
                   <div class="col-12 my-3">
                     <label class="form-label">內容編輯</label>
-
-                    <div id="editor"></div>
+                    <div id="editor" v-html="currentFaq.content"></div>
                   </div>
                   <div class="col-6">
                     <label class="form-label">狀態</label>
@@ -199,23 +243,36 @@ export default {
     </div>
 
     <!-- Error Modal -->
-    <div class="modal fade" :class="{ show: showModal }" tabindex="-1" :style="{ display: showModal ? 'block' : 'none' }">
+    <div
+      class="modal fade"
+      :class="{ show: showModal }"
+      tabindex="-1"
+      :style="{ display: showModal ? 'block' : 'none' }"
+    >
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title">錯誤提示</h5>
-            <button type="button" class="btn-close" @click="showModal = false"></button>
+            <button
+              type="button"
+              class="btn-close"
+              @click="showModal = false"
+            ></button>
           </div>
-          <div class="modal-body" v-html="modalMessage">
-          </div>
+          <div class="modal-body" v-html="modalMessage"></div>
           <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" @click="showModal = false">關閉</button>
+            <button
+              type="button"
+              class="btn btn-secondary"
+              @click="showModal = false"
+            >
+              關閉
+            </button>
           </div>
         </div>
       </div>
     </div>
     <div class="modal-backdrop fade show" v-if="showModal"></div>
-
   </div>
 </template>
 
