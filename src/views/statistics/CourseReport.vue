812c1806f17056d3a43b047384896a99d7527dc1
mafiapatch <g8071107@gmail.com>
2025-04-18 22:33:32 +0800

    style: 移除CourseReport.vue中的多余空行 ```
    解释：这个提交移除了`CourseReport.vue`文件中的一个多余空行，属于代码风格上的优化，因此使用`style`类型。

--- src/views/statistics/CourseReport.vue
+++ src/views/statistics/CourseReport.vue
@@ -121,7 +121,6 @@ export default {
         // 呼叫API取得學生資料
         const response = await apiService.getCourseStudent(courseId, studentId);
 
-
         // 整理API回傳的資料
         selectedStudent.value = {
           id: studentId,
